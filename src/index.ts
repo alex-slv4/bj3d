d8a33e69efdace02fc21fe62cb90c31e781dde60
Alexander Slavschik <kvinty@icloud.com>
2019-02-26 00:29:42 +0300

    babylonjs fix

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,18 @@ import Nullable = BABYLON.Nullable;
 import {log_warn} from "./log";
 import {CoreTypes} from "./CoreTypes";
 
-let engine: Engine;
-let scene: Scene;
-
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 canvas.setAttribute("touch-action", "none"); // for pepjs support
 canvas.width = window.innerWidth; // * window.devicePixelRatio;
 canvas.height = window.innerHeight; // * window.devicePixelRatio;
 document.body.appendChild(canvas);
 
-engine = new Engine(canvas, true, {}, true);
+const engine = new Engine(canvas, true, {}, true);
 
 canvas.style.width = "100%";
 canvas.style.height = "100%";﻿﻿
 
-scene = new Scene(engine);
+const scene = new Scene(engine);
 di.bind("canvas").toConstantValue(canvas);
 di.bind(Scene).toConstantValue(scene);
 
